cbccec72d64745427db624a7b889d3c07319c6e3
Andrew Hardwick <drew.hardwick@gmail.com>
2022-12-06 05:34:58 -0600

    Day6 complete

---
+++ day06/part1.py
@@ -0,0 +1,22 @@
+# part1.py
+
+
+def find_first_unique_set(data, window_length):
+	for i, w_s in enumerate(range(len(data) - (window_length - 1))):
+		if len(set(data[w_s:w_s + window_length])) == window_length:
+			return i + window_length
+
+	return -1
+
+def main(infn, window_length):
+	with open(infn, 'r') as f:
+		data = str.strip(f.readline())
+
+	result = find_first_unique_set(data, window_length)
+
+	print(result)
+
+if __name__ == '__main__':
+	main('test1.txt', 4)
+	main('test2.txt', 4)
+	main('input.txt', 4)
---
+++ day06/part2.py
@@ -0,0 +1,9 @@
+# part1.py
+
+from part1 import main
+
+
+if __name__ == '__main__':
+	main('test1.txt', 14)
+	main('test2.txt', 14)
+	main('input.txt', 14)
